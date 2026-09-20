73a56a173590e92484a5042508637e2cb8937e42
Zoqvera Desenvolvimento Web <handinhandengl@gmail.com>
2026-09-20 00:22:50 -0300

    seo: make Googlebot crawl policy explicit

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
         userAgent: "*",
         allow: "/",
       },
