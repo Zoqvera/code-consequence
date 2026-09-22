95f5af9c2a1b64df9c2260b540303646599d7ef0
Zoqvera Desenvolvimento Web <handinhandengl@gmail.com>
2026-09-22 10:30:26 -0300

    SEO: point robots to cross-site sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,9 @@ import { absoluteUrl } from "@/lib/seo";
 
 export const dynamic = "force-static";
 
+const crossSiteSitemapUrl =
+  "https://teacherflavius.com/sitemaps/code-consequence/sitemap.xml";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
@@ -23,6 +26,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: absoluteUrl("/sitemap.xml"),
+    sitemap: crossSiteSitemapUrl,
   };
 }
